0157a6b5b6a3907a59ac4444880f834e6f9e4fe4
daniirivv <danielriveranunezdearenas@outlook.com>
2025-12-27 13:07:13 +0100

    Add paths in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Preferences } from './pages/preferences/preferences';
+import { Game } from './pages/game/game';
+import { Records } from './pages/records/records';
+
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
+import { Logout } from './pages/logout/logout';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  // Sin slash; ruta relativa
+  { path: 'home', component: Home },
+  { path: 'preferences', component: Preferences },
+  { path: 'game', component: Game },
+  { path: 'records', component: Records },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'logout', component: Logout },
+
+  // Comodín: siempre al final (Angular lee por orden, como en CSS)
+  { path: '**', redirectTo: '/home' },
+];

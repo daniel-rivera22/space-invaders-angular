8cd5063d2d9573d750b29651af3bb70a6a7f92a4
daniirivv <danielriveranunezdearenas@outlook.com>
2025-12-27 18:07:05 +0100

    Add static register form

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-
+  registerForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 }

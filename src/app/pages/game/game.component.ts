964776c3709360f50e2b5cf446125d508c04681d
daniirivv <danielriveranunezdearenas@outlook.com>
2026-01-04 17:58:31 +0100

    feat: functional ship placeholder

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OnInit, AfterViewInit, OnDestroy, inject} from '@angular/core';
+import {ElementRef, ViewChild} from '@angular/core'
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -6,6 +9,117 @@ import { Component } from '@angular/core';
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
-export class Game {
+export class Game implements OnInit, AfterViewInit, OnDestroy {
+  private readonly gameService = inject(GameService);
 
+  /*
+  - @ViewChild es la forma "correcta" de obtener la referencia a un objeto del DOM; usa la etiqueta del '#'.
+  - ElementRef es un tipo envoltorio de seguridad que encapsula la referencia (como Optional<T> en Java)
+  */
+
+  @ViewChild('gameContainer') containerRef!: ElementRef<HTMLElement>;
+  @ViewChild('gameCanvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+  
+  private ctx!: CanvasRenderingContext2D;
+  private loopId = 0; // Necesario para parar el juego
+
+  private assets: Record<string, HTMLImageElement> = {};
+
+  ngOnInit(): void {
+    this.loadAssets();
+  }
+
+  private loadAssets() {
+    this.assets['ship'] = this.createHTMLImage('assets/ship.png');
+    this.assets['ufo'] = this.createHTMLImage('assets/ufo.png');
+  }
+
+  private createHTMLImage(path: string): HTMLImageElement {
+    const img = new Image();
+    img.src = path;
+    return img;
+  }
+
+  ngAfterViewInit(): void {
+    const container = this.containerRef.nativeElement;
+    const canvas = this.canvasRef.nativeElement;
+
+    canvas.width = container.clientWidth;
+    canvas.height = container.clientHeight;
+
+    this.ctx = canvas.getContext('2d')!;
+
+    this.gameService.setUp(canvas.width, canvas.height);
+
+    canvas.focus(); // Para no tener que hacer clic en él
+
+    this.gameLoop();
+  }
+
+  ngOnDestroy(): void {
+    if (this.loopId) cancelAnimationFrame(this.loopId);
+  }
+
+  onKeyDown(ke: KeyboardEvent) {
+    switch (ke.key) {
+      case 'ArrowLeft':
+        this.gameService.setMoveLeft(true);
+        break;
+
+      case 'ArrowRight':
+        this.gameService.setMoveRight(true);
+        break;
+
+      case 'Spacebar':
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  onKeyUp(ke: KeyboardEvent) {
+    // Nota: para flechas y espacio da igual, pero mejor usar .code (universal) que .key (cambia con el teclado)
+    switch (ke.code) {
+      case 'ArrowLeft':
+        this.gameService.setMoveLeft(false);
+        break;
+
+      case 'ArrowRight':
+        this.gameService.setMoveRight(false);
+        break;
+
+      case 'Spacebar':
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  /*
+    Se usa arrow function ( ()=>{} ) porque mantiene el "this" constante; siempre es GameComponent, independientemente de quién llame a la función
+    Es más limpio que usar .bind(this)
+  */
+  private gameLoop = () => {
+    this.gameService.update();
+    this.draw();
+    this.loopId = requestAnimationFrame(this.gameLoop); // this siempre será esta la instancia de GameComponent
+  };
+
+  private draw() {
+    const canvasWidth = this.canvasRef.nativeElement.width;
+    const canvasHeight = this.canvasRef.nativeElement.height;
+
+    this.ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    const ship = this.gameService.getShip();
+
+    if (ship && this.assets['ship']) {
+      this.ctx.fillStyle = '#00FF00'; // Fija el estilo de relleno del canva
+      this.ctx.fillRect(ship.x, ship.y, ship.width, ship.height); // Rellena el rect(angle)
+
+      // this.ctx.drawImage(this.assets['ship'], ship.x, ship.y, ship.width, ship.height);
+    }
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,11 +1,92 @@
 import { Injectable } from '@angular/core';
-import { Ship } from '../models/game-models';
+import { Ship, Ufo as UFOS } from '../models/game-models';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class GameService {
+  
   public width: number = 0;
   public height: number = 0;
+
+  /*
+    Usar ! le dice a TS que se "fíe" de que se va a inicializar la variable desde fuera
+    Sirve para no inicializar un objeto con datos falsos solo para satisfacer al compilador
+  */
+  private ship!: Ship;
+  private readonly ufo: UFOS[] = [];
+
+  private score: number = 0;
+  private timeLeft: number;
+
+  // Flags de teclado, para evitar el retardo al mantener pulsado
+  private leftArrowPressedFlag: boolean = false;
+  private righArrowPressedFlag: boolean = false;
+
+  constructor() {}
+
+  getShip(): Ship {
+    return this.ship;
+  }
+
+  getScore(): number {
+    return this.score;
+  }
+
+  getTime(): number {
+    return this.timeLeft;
+  }
+
+  setMoveLeft(state: boolean) {
+    this.leftArrowPressedFlag = state;
+  }
+
+  setMoveRight(state: boolean) {
+    this.righArrowPressedFlag = state;
+  }
+
+  setUp(canvasWidth: number, canvasHeight: number) {
+    this.width = canvasWidth;
+    this.height = canvasHeight;
+    this.launchGame();
+  }
+
+  private launchGame() {
+    this.initializeShip();
+  }
+
+  private initializeShip(): void {
+    // IMPORTANTE: Los ejes del canvas comienzan en la esquina superior izquierda, y crecen a la derecha (x) y abajo (y)
+    const centerX = this.width / 2 - this.SHIP_WIDTH / 2;
+    const centerWithBottomMarginY = this.height - this.SHIP_HEIGHT - this.SHIP_MARGIN_BOTTOM;
+
+    this.ship = {
+      width: this.SHIP_WIDTH,
+      height: this.SHIP_HEIGHT,
+      x: centerX,
+      y: centerWithBottomMarginY,
+      speed: this.SHIP_SPEED,
+    };
+  }
+
+  private initializeUfos(): void {
+    this.ufos = [];
+  }
+
+  update() {
+    // Early return; evita sobrecargar el código de tabulaciones
+    if (!this.ship || this.width === 0) return;
+
+    this.updateShipCoords();
+  }
+
+  private updateShipCoords(): void {
+    if (this.leftArrowPressedFlag && this.ship.x > 0) {
+      this.ship.x -= this.ship.speed;
+    }
+
+    if (this.righArrowPressedFlag && this.ship.x + this.ship.width < this.width) {
+      this.ship.x += this.ship.speed;
+    }
+  }
 }

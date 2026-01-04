67a50b6ce3913c766149ca8bc384c6901878fb99
daniirivv <danielriveranunezdearenas@outlook.com>
2026-01-04 17:57:14 +0100

    add: config object to cleaning game service

--- src/app/models/game-models.ts
+++ src/app/models/game-models.ts
@@ -3,10 +3,11 @@ export interface Entity {
     y: number;
     width: number;
     height: number;
+    speed: number;
 }
 
 export interface Ship extends Entity {
-    velocidad: number;
+    
 }
 
 export interface Bullet extends Entity {
@@ -14,6 +15,28 @@ export interface Bullet extends Entity {
 }
 
 export interface Ufo extends Entity {
+    direction: number;
     hit: boolean;
 }
 
+/*
+  Encapsula todas las variables relacionadas con constantes de los elementos del juego
+  Además, es más limpio que dejar las constantes sueltas, y descarga el GameService de estas constantes.
+*/
+export const GAME_CONFIG = {
+  SHIP: {
+    WIDTH: 30,
+    HEIGHT: 50,
+    SRC: 'assets/ship.png',
+  },
+  UFO: {
+    WIDTH: 40,
+    HEIGHT: 40,
+    SRC: 'assets/ufo.png',
+  },
+  BULLET: {
+    WIDTH: 2,
+    HEIGHT: 5,
+    COLOR: 'red',
+  }
+};
